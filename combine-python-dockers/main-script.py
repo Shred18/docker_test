3355cde6d4f19bcc91b259bd5ed8158634fad67a
Cory Shreffler <58868390+Shred18@users.noreply.github.com>
2020-05-17 15:29:17 -0500

    Update main-script.py

--- combine-python-dockers/main-script.py
+++ combine-python-dockers/main-script.py
@@ -60,5 +60,5 @@ def running():
 
 
 if __name__ == "__main__":
-    #app.run(debug=True,host="0.0.0.0",port=int("4500"))
-    app.run()
+    app.run(debug=True,host="0.0.0.0",port=int("4500"))
+    #app.run()
